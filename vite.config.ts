a20e39a4d87bd47856eb92a3d7e28901b89478bc
Serchman <Maksymilian.dev@gmail.com>
2026-07-01 12:57:21 +0200

    Set base path for build command in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/pseudoku/' : '/',
   plugins: [svelte()],
   test: {
     environment: 'node',
   },
-});
+}));
